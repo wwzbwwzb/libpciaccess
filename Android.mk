8a56811a00dec708316b716bdc91e6da5c3e0f68
daafvv <daafvv@gmail.com>
2012-12-03 14:08:50 +0800

    add Android.mk; add config.h pciaccess.h

---
+++ Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH :=$(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=			\
+src/common_bridge.c \
+src/common_capability.c \
+src/common_device_name.c \
+src/common_init.c \
+src/common_interface.c \
+src/common_iterator.c \
+src/common_map.c \
+src/linux_devmem.c \
+src/linux_sysfs.c \
+src/scanpci.c 
+
+LOCAL_CFLAGS := -Dlinux=1
+LOCAL_C_INCLUDES +=  $(LOCAL_PATH)/src
+
+LOCAL_MODULE := libpciaccess
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
